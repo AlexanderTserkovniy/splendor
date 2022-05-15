f866d0e342415c1265c95360dbbcb7247db5b91d
Oleksandr Tserkovnyi <kemperomg@gmail.com>
2022-05-15 00:36:12 -0400

    test: fix tests

--- src/components/StartWizard/StartWizard.test.tsx
+++ src/components/StartWizard/StartWizard.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from '../../redux/store';
+import store from '../../redux/store';
 import StartWizard from './StartWizard';
 
 test('renders start wizard text', () => {
