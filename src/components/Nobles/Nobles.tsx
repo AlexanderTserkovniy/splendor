1cce51b3c1782ae6482147c9993145bde1edbfcc
Oleksandr Tserkovnyi <kemperomg@gmail.com>
2022-06-26 14:44:43 -0400

    feat: working on noble view

--- src/components/Nobles/Nobles.tsx
+++ src/components/Nobles/Nobles.tsx
@@ -1,15 +1,40 @@
 import { nobles } from '../../resources/cards.json';
+// TODO Use it to detect correct names and move names to common place
+// import { tokenNames } from '../../resources/config.json';
+import nobleDummy from '../../images/noble-dummy.jpeg';
+import styles from './Nobles.module.css';
 
-const Nobles = ({ ...props } : { props?: any }) => (
-  <section>
-    <header>
-      <h2>Nobles:</h2>
-    </header>
+const Nobles = ({ ...props } : { props?: any }) =>
+  (
+    <section {...props}>
+      <header>
+        <h2>Nobles:</h2>
+      </header>
 
-    <article>
-      {JSON.stringify(nobles)}
-    </article>
-  </section>
-)
+      <article>
+        <ul className={styles.Nobles}>
+          {nobles.map(({price}) => (
+            <li className={styles.Noble} key={Math.random()}>
+              <header className={styles['Noble-price']}>
+                <ul className={styles['price-wrapper']}>
+                  {
+                    Object.entries(price).map(([tokenName, tokenValue]) => (
+                      <li className={styles.price} style={{ background: tokenName }}>
+                        {tokenValue}
+                      </li>
+                    ))
+                  }
+                </ul>
+              </header>
+              <footer className={styles['Noble-image']}>
+                <img src={nobleDummy} alt="Noble face and price" className={styles.image}/>
+              </footer>
+            </li>
+          ))}
+        </ul>
+        {JSON.stringify(nobles)}
+      </article>
+    </section>
+  )
 
 export default Nobles;
